64e395fd75fcdab58bd710a970be4b98331523e1
Nech99 <anadischolastica@gmail.com>
2022-07-05 18:37:59 -0700

    absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _abs - absolute value
+ * @n: integer
+ *
+ * Return: return absolute valuer of a number
+ */
+int _abs(int v)
+{
+	if (v < 0)
+	{
+		v = v * -1;
+		return (v);
+	}
+	else
+	{
+		return (v);
+	}
+}
