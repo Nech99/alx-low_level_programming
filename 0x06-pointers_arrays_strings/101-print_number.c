0fc260f647d52809ab18767361d5cae963b9319d
Nech99 <anadischolastica@gmail.com>
2022-07-14 20:37:52 -0700

    prints int

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -1,30 +1,22 @@
-#include <stdio.h>
+#include "main.h"
 
 /**
- * main - computes and prints the sum of all the multiples
- *  * of 3 or 5 below 1024
- * Return: Always 0 (Success)
- *
+ * print_number - Prints an integer.
+ * @n: The integer to be printed.
  */
-int main(void)
+
+void print_number(int n)
 {
-	unsigned long int sum3, sum5, sum;
-		int i;
-	sum3 = 0;
-	sum5 = 0;
-	sum = 0;
-	for (i = 0; i < 1024; ++i)
+	unsigned int num = n;
+
+	if (n < 0)
 	{
-		if ((i % 3) == 0)
-		{
-			sum3 = sum3 + i;
-		}
-		else if ((i % 5) == 0)
-		{
-			sum5 = sum5 + i;
-		}
+		_putchar('-');
+		num = -num;
 	}
-		sum = sum3 + sum5;
-		printf("%lu\n", sum);
-		return (0);
+
+	if ((num / 10) > 0)
+		print_number(num / 10);
+
+	_putchar((num % 10) + '0');
 }
