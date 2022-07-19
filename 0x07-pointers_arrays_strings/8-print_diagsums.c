ae4993551ad830cb0020aeb0e52271a93fd82b40
Nech99 <anadischolastica@gmail.com>
2022-07-18 17:46:27 -0700

    diagsums

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_diagsums - prints thee sum of two diagonals 
+ 		    of a square matrix of integers
+ * @a: func para
+ * @size: second func para
+ * Return: void
+ */
+
+void print_diagsums(int *a, int size)
+{
+}
