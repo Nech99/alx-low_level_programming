0cc7ac1523a53d810d45658d125b3a35b115ed1a
Nech99 <anadischolastica@gmail.com>
2022-07-20 20:16:28 -0700

    recursion in c

--- 0x08-recursion.c/main.h
+++ 0x08-recursion.c/main.h
@@ -1,6 +1,7 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <math.h>
 int _putchar(char c);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
@@ -8,6 +9,9 @@ int _strlen_recursion(char *s);
 int facorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
+int sqt(int m, int d);
 int is_prime_number(int n);
 
 #endif
+Footer
+
