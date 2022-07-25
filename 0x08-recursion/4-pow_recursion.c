cee28913f8700f8b96078935aff74a472fd2d999
Nech99 <anadischolastica@gmail.com>
2022-07-25 08:08:38 -0700

    hot fix

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,21 +1,23 @@
 #include "main.h"
-#include <math.h>
-/**
- * _pow_recursion - returns a power value
- * @x: func para
- * @y: second func para
- * Return: power
- */
 
+/**
+ * _pow_recursion - Returns the value of x raised to the power of y.
+ * @x: The number to be raised.
+ * @y: The power.
+ *
+ * Return: The value of x raised to the power of y.
+ */
 int _pow_recursion(int x, int y)
 {
-	if (y == 0)
-	{
-		return (1);
-	}
-	else if (y < 0)
-	{
+	int result = x;
+
+	if (y < 0)
 		return (-1);
-	}
-	return (pow(x, y));
+
+	else if (y == 0)
+		return (1);
+
+	result *= _pow_recursion(x, y - 1);
+
+	return (result);
 }
