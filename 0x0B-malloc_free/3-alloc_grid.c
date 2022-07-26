684eea95fc0a6efe1a6bb2fdc57a336605e79d95
Nech99 <anadischolastica@gmail.com>
2022-07-26 13:37:51 -0700

    quick fix

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -1,38 +1,34 @@
 #include "main.h"
 #include <stdlib.h>
-#include <string.h>
-
 /**
- * str_concat -concatenates two stringss
- * @s1: first parameter to concatenate
- * @s2: second parameter to concatenate
- * Return: NULL or pointer to newly allocated space
+ * alloc_grid - returns a pointer to a 2 dimensional array of ints
+ * @width: width
+ * @height: heignt
+ * Return: NULL or pointer to 2 dim arr
  */
-
-char *str_concat(char *s1, char *s2)
+int **alloc_grid(int width, int height)
 {
-	char *str;
-	int i, j, len, len1;
+	int i, j, p, **chr;
 
-	if (s1 == NULL)
+	if (width <= 0 || height <= 0)
 	{
-		s1 = "";
+		return (NULL);
 	}
-	if (s2 == NULL)
+	chr = malloc(height * sizeof(*chr));
+	if (chr == NULL)
+		return (chr);
+	for (i = 0; i < height; i++)
 	{
-		s2 = "";
+		chr[i] = malloc(width * sizeof(int));
+		if (chr[i] == NULL)
+		{
+			for (p = 0; p < i; p++)
+				free(chr[p]);
+			free(chr);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			chr[i][j] = 0;
 	}
-	for (len = 0; s1[len]; len++)
-		;
-	for (len1 = 0; s2[len1]; len1++)
-		;
-	str = malloc(len + len1 + 1);
-	if (str == NULL)
-		return (str);
-	for (i = 0; i < len; i++)
-		str[i] = s1[i];
-	for (j = 0; i < len1 + len; i++, j++)
-		str[i] = s2[j];
-	str[i] = '\0';
-	return (str);
+	return (chr);
 }
